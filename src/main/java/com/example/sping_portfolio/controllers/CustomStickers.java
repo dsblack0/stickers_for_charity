0ed2f749f4d3866499ae76dbfb0c01e93f3a6b9c
KaavyaU <ftc13184@gmail.com>
2022-05-17 11:07:00 -0700

    Custom Stickers Email API

--- src/main/java/com/example/sping_portfolio/controllers/CustomStickers.java
+++ src/main/java/com/example/sping_portfolio/controllers/CustomStickers.java
@@ -6,6 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.text.ParseException;
 
 @Controller
@@ -18,6 +22,22 @@ public class CustomStickers {
                                  @RequestParam(name="description", required=false, defaultValue = "") String description,
                                  Model model)
             throws IOException, InterruptedException, ParseException {
+
+        //api
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://rapidprod-sendgrid-v1.p.rapidapi.com/mail/send"))
+                .header("content-type", "application/json")
+                .header("X-RapidAPI-Host", "rapidprod-sendgrid-v1.p.rapidapi.com")
+                .header("X-RapidAPI-Key", "5160560674msh71fe7bcad890509p159e0ajsnc0f73e8a4ad9")
+                .method("POST", HttpRequest.BodyPublishers.ofString("{\"personalizations\":[{\"to\":[{\"email\":\"kkutti@outlook.com\"}],\"subject\":\"Custom Sticker Order\"}],\"from\":{\"email\":\"from_address@example.com\"},\"content\":[{\"type\":\"text/plain\",\"value\":\"Hello, World!\"}]}"))
+                .build();
+        HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+
+        model.addAttribute("name", name);
+        model.addAttribute("email", email);
+        model.addAttribute("phone", phone);
+        model.addAttribute("description", description);
 
         return "/CustomStickers";
     }
