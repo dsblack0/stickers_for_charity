beda65c01d7ef31499a5fd4d75069e395994c1d3
KaavyaU <ftc13184@gmail.com>
2022-05-17 11:14:29 -0700

    Custom Stickers Email API

--- src/main/java/com/example/sping_portfolio/controllers/CustomStickers.java
+++ src/main/java/com/example/sping_portfolio/controllers/CustomStickers.java
@@ -29,7 +29,8 @@ public class CustomStickers {
                 .header("content-type", "application/json")
                 .header("X-RapidAPI-Host", "rapidprod-sendgrid-v1.p.rapidapi.com")
                 .header("X-RapidAPI-Key", "5160560674msh71fe7bcad890509p159e0ajsnc0f73e8a4ad9")
-                .method("POST", HttpRequest.BodyPublishers.ofString("{\"personalizations\":[{\"to\":[{\"email\":\"kkutti@outlook.com\"}],\"subject\":\"Custom Sticker Order\"}],\"from\":{\"email\":\"from_address@example.com\"},\"content\":[{\"type\":\"text/plain\",\"value\":\"Hello, World!\"}]}"))
+                .method("POST", HttpRequest.BodyPublishers.ofString
+                        ("{\r\n    \"personalizations\": [\r\n        {\r\n            \"to\": [\r\n                {\r\n                    \"email\": \"kkutti@outlook.com\"\r\n                }\r\n            ],\r\n            \"subject\": \"Hello, World!\"\r\n        }\r\n    ],\r\n    \"from\": {\r\n        \"email\": \"from_address@example.com\"\r\n    },\r\n    \"content\": [\r\n        {\r\n            \"type\": \"text/plain\",\r\n            \"value\": \"Hello, World!\"\r\n        }\r\n    ]\r\n}"))
                 .build();
         HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
         System.out.println(response.body());
