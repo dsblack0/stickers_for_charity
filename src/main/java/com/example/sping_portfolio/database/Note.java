20f386260960d8a2d5f6fc61ff0787bb4bea8c27
Prisha Maiti <89211997+PrishaMaiti@users.noreply.github.com>
2022-05-17 10:44:54 -0700

    Added Note Files for Database

---
+++ src/main/java/com/example/sping_portfolio/database/Note.java
@@ -0,0 +1,25 @@
+package com.example.sping_portfolio.database;
+
+import com.example.sping_portfolio.database.Person;
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Note {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name="person_id")
+    private Person person;
+
+    @NotNull
+    @Column(columnDefinition="TEXT")
+    private String text;
+}
