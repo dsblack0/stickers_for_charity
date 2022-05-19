2f215d882e525141ac17cfc2cc2be062db44a793
sam <70492417+dsblack0@users.noreply.github.com>
2022-05-19 09:33:43 -0700

    working on beans

--- src/main/java/com/example/sping_portfolio/controllers/aboutTeam/ProfileJpaRepository.java
+++ src/main/java/com/example/sping_portfolio/controllers/aboutTeam/ProfileJpaRepository.java
@@ -2,5 +2,6 @@ package com.example.sping_portfolio.controllers.aboutTeam;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface ProfileJpaRepository extends JpaRepository<com.example.sping_portfolio.controllers.aboutTeam.Profile, Long> {
 }
--- src/main/java/com/example/sping_portfolio/controllers/aboutTeam/ProfileSqlRepository.java
+++ src/main/java/com/example/sping_portfolio/controllers/aboutTeam/ProfileSqlRepository.java
@@ -1,6 +1,8 @@
 package com.example.sping_portfolio.controllers.aboutTeam;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
