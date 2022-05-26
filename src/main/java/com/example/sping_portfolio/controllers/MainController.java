f4d5b250bb871d261f84f0223b54b7417674811c
Prisha Maiti <89211997+PrishaMaiti@users.noreply.github.com>
2022-05-25 19:23:21 -0700

    Signup Database Main Work (First Redirect)

--- src/main/java/com/example/sping_portfolio/controllers/MainController.java
+++ src/main/java/com/example/sping_portfolio/controllers/MainController.java
@@ -1,13 +1,15 @@
 package com.example.sping_portfolio.controllers;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
 import java.util.ArrayList;
-import java.util.Objects;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
@@ -36,5 +38,46 @@ public class MainController {
     @GetMapping("/login")
     public String login(Model model) {return "login";}
 
+    @GetMapping("/person")
+    public String person(Model model) {return "person";}
+
+    @GetMapping("/personcreate")
+    public String personcreate(Model model) {return "personcreate";}
+
+    @GetMapping("/personupdate")
+    public String personupdate(Model model) {return "personupdate";}
+
+    // these are for later
+    /* @GetMapping("/")
+    public String process(Model model, HttpSession session) {
+        @SuppressWarnings("unchecked")
+        List<String> messages = (List<String>) session.getAttribute("MY_SESSION_MESSAGES");
+
+        if (messages == null) {
+            messages = new ArrayList<>();
+        }
+        model.addAttribute("sessionMessages", messages);
+
+        return "index";
+    }
+
+    @PostMapping("/persistMessage")
+    public String persistMessage(@RequestParam("msg") String msg, HttpServletRequest request) {
+        @SuppressWarnings("unchecked")
+        List<String> messages = (List<String>) request.getSession().getAttribute("MY_SESSION_MESSAGES");
+        if (messages == null) {
+            messages = new ArrayList<>();
+            request.getSession().setAttribute("MY_SESSION_MESSAGES", messages);
+        }
+        messages.add(msg);
+        request.getSession().setAttribute("MY_SESSION_MESSAGES", messages);
+        return "redirect:/";
+    }
+
+    @PostMapping("/destroy")
+    public String destroySession(HttpServletRequest request) {
+        request.getSession().invalidate();
+        return "redirect:/";
+    } */
 }
 
